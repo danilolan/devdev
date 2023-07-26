0ba0dd8e351a030ab881c3fb1894efa470e6c4fd
danilo <didoherculano@gmail.com>
2023-07-26 00:40:10 -0300

    feat: simple camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
+mod camera;
+use camera::CameraPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        // Window Setup
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::new())
+        
+        //Plugins
+        .add_plugins(CameraPlugin)
+        .run();
 }
