e4c8a99629c48ba0de55673ab0d656bfa57baa9e
danilo <didoherculano@gmail.com>
2023-08-04 01:51:31 -0300

    feat: plane movement

--- src/camera.rs
+++ src/camera.rs
@@ -1,22 +1,59 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform, render::camera};
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        app.add_systems(Startup, spawn_player);
+        app.add_systems(Update, plane_movement);
     }
 }
 
-pub fn spawn_camera(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+#[derive(Component)]
+pub struct Player {
+    transform: Transform
+}
+
+pub fn spawn_player(
+    mut commands: Commands
 ){
-    commands.spawn(
+    commands.spawn(Player {
+        transform: Transform::from_xyz(-0.0, 5.3, 11.5)
+    })
+    .insert(
         Camera3dBundle {
-            transform: Transform::from_xyz(-0.0, 5.3, 11.5).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-5.0, 5.0, 5.0)
+            .looking_at(Vec3::ZERO, Vec3::new(0.0, 1.0, 0.0)),
             ..default()  
-        }
+        },
     );
+}
+
+pub fn plane_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>
+){
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 1.0)
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, -1.0)
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(-1.0,0.0 ,1.0)
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(1.0, 0.0, -1.0)
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation +=  direction * 5.0 * time.delta_seconds();
+    }
 }
