a365e1eeb090577723d54006eab2ba2a7f3f500d
danilo <didoherculano@gmail.com>
2023-08-27 00:40:50 -0300

    refactor: remove walls struct

--- src/world/walls.rs
+++ src/world/walls.rs
@@ -12,7 +12,6 @@ impl Plugin for WallsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, start_wall);
         app.init_resource::<WallPoints>();
-        app.init_resource::<Walls>();
     }
 }
 
@@ -27,52 +26,112 @@ impl Default for WallPoints {
         WallPoints { points: Vec::new() }
     }
 }
+//----components----
+#[derive(Component)]
+struct WallMesh {}
 
-//structs
-#[derive(Resource)]
-struct Walls {
-    height: f32,
-    size: f32,
-    pub meshes: Option<Handle<Mesh>>,
+//----systems----
+fn start_wall(
+    mut commands: Commands,
+    gizmos: Gizmos,
+    mut res_mesh: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let points = [
+        [Vec3::new(0.0, 0.0, 0.0), Vec3::new(10.0, 0.0, 0.0)],
+        [Vec3::new(10.0, 0.0, 0.0), Vec3::new(10.0, 0.0, 10.0)],
+    ];
+
+    for point in points {}
+    let mesh = add_wall(
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::new(10.0, 0.0, 5.0),
+        1.0,
+        2.0,
+        res_mesh,
+    );
+
+    let material = materials.add(Color::rgb(0.2, 0.2, 0.2).into());
+    commands.spawn(PbrBundle {
+        mesh,
+        material,
+        ..Default::default()
+    });
 }
 
-impl Default for Walls {
-    fn default() -> Self {
-        Walls {
-            height: 2.0,
-            size: 1.0,
-            meshes: None,
+struct MeshBuilder {
+    vertices: Vec<Vec3>,
+    indices: Vec<u32>,
+    normals: Vec<Vec3>,
+}
+
+impl MeshBuilder {
+    pub fn new() -> Self {
+        MeshBuilder {
+            vertices: Vec::new(),
+            indices: Vec::new(),
+            normals: Vec::new(),
         }
     }
+
+    pub fn add_square(&mut self, p0: Vec3, p1: Vec3, p2: Vec3, p3: Vec3) {
+        // Os vértices são adicionados à lista
+        let start_index = self.vertices.len() as u32;
+        self.vertices.push(p0);
+        self.vertices.push(p1);
+        self.vertices.push(p2);
+        self.vertices.push(p3);
+
+        // Cálculo das normais (supondo que os pontos são coplanares e em sentido horário)
+        let normal = (p1 - p0).cross(p2 - p0).normalize();
+
+        self.normals.push(normal);
+        self.normals.push(normal);
+        self.normals.push(normal);
+        self.normals.push(normal);
+
+        // Adicionando índices para formar o quadrado como dois triângulos
+        // Triângulo 1: p0, p1, p2
+        // Triângulo 2: p0, p2, p3
+        self.indices.push(start_index);
+        self.indices.push(start_index + 1);
+        self.indices.push(start_index + 2);
+        self.indices.push(start_index);
+        self.indices.push(start_index + 2);
+        self.indices.push(start_index + 3);
+    }
+
+    pub fn build(&self) -> Mesh {
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, self.vertices.clone());
+        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, self.normals.clone());
+        mesh.set_indices(Some(Indices::U32(self.indices.clone())));
+        mesh
+    }
 }
 
-impl Walls {
-    //0-1-5-0-5-4,2-3-6-2-6-7,1-3-6-1-6-5
-    pub fn add_wall(
-        &mut self,
-        start: Vec3,
-        end: Vec3,
-        mut meshes: ResMut<Assets<Mesh>>,
-        mut gizmos: Gizmos,
-    ) -> Handle<Mesh> {
-        let size = self.size;
-        let height = self.height;
-        let points = calc_wall_points(start, end, size, height);
-        let indices = [0, 1, 5, 0, 5, 4, 2, 3, 6, 2, 6, 7, 1, 3, 6, 1, 6, 5];
+pub fn add_wall(
+    start: Vec3,
+    end: Vec3,
+    size: f32,
+    height: f32,
+    mut meshes: ResMut<Assets<Mesh>>,
+) -> Handle<Mesh> {
+    let points = calc_wall_points(start, end, size, height);
+    let indices = [0, 1, 5, 0, 5, 4, 2, 3, 6, 2, 6, 7, 1, 3, 6, 1, 6, 5];
 
-        let mut builder = MeshBuilder::new();
+    let mut builder = MeshBuilder::new();
 
-        // Adicionando o primeiro quadrado
-        builder.add_square(points[6], points[7], points[3], points[2]);
-        builder.add_square(points[1], points[3], points[7], points[5]);
-        builder.add_square(points[0], points[1], points[5], points[4]);
-        let mesh = builder.build();
+    // Adicionando o primeiro quadrado
+    builder.add_square(points[6], points[7], points[3], points[2]);
+    builder.add_square(points[1], points[3], points[7], points[5]);
+    builder.add_square(points[0], points[1], points[5], points[4]);
 
-        let mesh_handle = meshes.add(mesh);
-        self.meshes = Some(mesh_handle.clone());
+    let mesh = builder.build();
 
-        mesh_handle
-    }
+    let mesh_handle = meshes.add(mesh);
+
+    mesh_handle
 }
 
 fn calc_wall_points(start: Vec3, end: Vec3, size: f32, height: f32) -> [Vec3; 8] {
@@ -127,84 +186,4 @@ fn calc_perpendicular_points(point: Vec3, direction: Vec3, size: f32) -> (Vec3, 
     let point_right = point - perpendicular_point;
 
     (point_right, point_left)
-}
-
-//----components----
-#[derive(Component)]
-struct WallMesh {}
-
-//----systems----
-fn start_wall(
-    mut walls: ResMut<Walls>,
-    mut commands: Commands,
-    gizmos: Gizmos,
-    mut res_mesh: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    walls.add_wall(
-        Vec3::new(0.0, 0.0, 0.0),
-        Vec3::new(10.0, 0.0, 0.0),
-        res_mesh,
-        gizmos,
-    );
-
-    let material = materials.add(Color::rgb(0.2, 0.2, 0.2).into());
-    if let Some(mesh) = walls.meshes.clone() {
-        commands.spawn(PbrBundle {
-            mesh,
-            material,
-            ..Default::default()
-        });
-    }
-}
-
-struct MeshBuilder {
-    vertices: Vec<Vec3>,
-    indices: Vec<u32>,
-    normals: Vec<Vec3>,
-}
-
-impl MeshBuilder {
-    pub fn new() -> Self {
-        MeshBuilder {
-            vertices: Vec::new(),
-            indices: Vec::new(),
-            normals: Vec::new(),
-        }
-    }
-
-    pub fn add_square(&mut self, p0: Vec3, p1: Vec3, p2: Vec3, p3: Vec3) {
-        // Os vértices são adicionados à lista
-        let start_index = self.vertices.len() as u32;
-        self.vertices.push(p0);
-        self.vertices.push(p1);
-        self.vertices.push(p2);
-        self.vertices.push(p3);
-
-        // Cálculo das normais (supondo que os pontos são coplanares e em sentido horário)
-        let normal = (p1 - p0).cross(p2 - p0).normalize();
-
-        self.normals.push(normal);
-        self.normals.push(normal);
-        self.normals.push(normal);
-        self.normals.push(normal);
-
-        // Adicionando índices para formar o quadrado como dois triângulos
-        // Triângulo 1: p0, p1, p2
-        // Triângulo 2: p0, p2, p3
-        self.indices.push(start_index);
-        self.indices.push(start_index + 1);
-        self.indices.push(start_index + 2);
-        self.indices.push(start_index);
-        self.indices.push(start_index + 2);
-        self.indices.push(start_index + 3);
-    }
-
-    pub fn build(&self) -> Mesh {
-        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, self.vertices.clone());
-        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, self.normals.clone());
-        mesh.set_indices(Some(Indices::U32(self.indices.clone())));
-        mesh
-    }
 }
