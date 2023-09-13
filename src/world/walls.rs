e2b99628c752f45cac1871a53244354c8d707e27
danilo <didoherculano@gmail.com>
2023-09-12 22:25:11 -0300

    feat: wallpoints struct with points and connection

--- src/world/walls.rs
+++ src/world/walls.rs
@@ -16,41 +16,6 @@ impl Plugin for WallsPlugin {
     }
 }
 
-//----resources----
-#[derive(Resource)]
-pub struct WallPoints {
-    line: Option<[[i32; 2]; 2]>,
-}
-
-impl WallPoints {
-    pub fn add_line(&mut self, line: [[i32; 2]; 2]) {
-        let [x1, y1] = line[0];
-        let [x2, y2] = line[1];
-
-        // Calcular a distância em x e y
-        let dx = x2 - x1;
-        let dy = y2 - y1;
-
-        let point2: [i32; 2];
-        if dx.abs() < dy.abs() {
-            point2 = [x1, y2]
-        } else {
-            point2 = [x2, y1]
-        }
-
-        self.line = Some([line[0], point2]);
-    }
-}
-
-impl Default for WallPoints {
-    fn default() -> Self {
-        WallPoints { line: None }
-    }
-}
-//----components----
-#[derive(Component)]
-struct WallMesh {}
-
 //----systems----
 fn start_walls(mut commands: Commands) {
     commands
@@ -69,18 +34,20 @@ fn handle_walls(
     }
 
     let wall: Handle<Scene> = server.load("./models/wall.gltf#Scene0");
+    let pillar: Handle<Scene> = server.load("./models/pillar.gltf#Scene0");
 
-    if let Some(points) = walls_points.line {
+    if let Some(points) = &walls_points.line {
         let start = grid.coord_to_tile(points[0]);
         let end = grid.coord_to_tile(points[1]);
         let object_size = 0.4;
         let scale_multiplier = 2.5;
 
         let direction = end - start;
-        let scale = direction.length() - object_size;
+        let scale = direction.length();
         let rotation = Quat::from_rotation_arc(Vec3::X, direction.normalize());
         let midpoint = start + (direction / 2.0);
 
+        //wall
         commands
             .spawn(
                 (SceneBundle {
@@ -98,4 +65,104 @@ fn handle_walls(
 
         walls_points.line = None;
     }
+
+    //pillars
+    for point in &walls_points.pillars {
+        let translation = grid.coord_to_tile(*point);
+
+        commands
+            .spawn(
+                (SceneBundle {
+                    scene: pillar.clone(),
+                    transform: Transform {
+                        translation,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                }),
+            )
+            .insert(Name::from("pillar".to_string()));
+    }
+    walls_points.pillars.clear();
 }
+
+//----resources----
+#[derive(Resource)]
+pub struct WallPoints {
+    line: Option<[[i32; 2]; 2]>,
+    pillars: Vec<[i32; 2]>,
+    points: Vec<[i32; 2]>,
+    connections: Vec<[usize; 2]>,
+}
+
+impl WallPoints {
+    // Function to calculate the end point based on the initial point and the differences dx and dy
+    fn calculate_point_end(line: [[i32; 2]; 2]) -> [i32; 2] {
+        let [x1, y1] = line[0];
+        let [x2, y2] = line[1];
+
+        let dx = x2 - x1;
+        let dy = y2 - y1;
+
+        if dx.abs() < dy.abs() {
+            [x1, y2]
+        } else {
+            [x2, y1]
+        }
+    }
+
+    // Function to check if two points are equal
+    fn points_are_equal(point1: [i32; 2], point2: [i32; 2]) -> bool {
+        point1 == point2
+    }
+
+    // Function to add points and pillars if they don't already exist in the respective vectors
+    fn add_points_and_pillars(&mut self, point_start: [i32; 2], point_end: [i32; 2]) {
+        for &point in &[point_start, point_end] {
+            if !self.points.contains(&point) {
+                self.points.push(point);
+                self.pillars.push(point);
+            }
+        }
+    }
+
+    pub fn add_line(&mut self, line: [[i32; 2]; 2]) {
+        let point_start = line[0];
+
+        // Using the helper function to calculate the end point
+        let point_end = Self::calculate_point_end(line);
+
+        // Using the helper function to check if the points are equal
+        if Self::points_are_equal(point_start, point_end) {
+            return;
+        }
+
+        // Using the helper function to add points and pillars
+        self.add_points_and_pillars(point_start, point_end);
+
+        // Find indices of start and end points in the points vector
+        if let Some(index_start) = self.points.iter().position(|&x| x == point_start) {
+            if let Some(index_end) = self.points.iter().position(|&x| x == point_end) {
+                if !self.connections.contains(&[index_start, index_end]) {
+                    self.line = Some([point_start, point_end]);
+                    self.connections.push([index_start, index_end]);
+                }
+            }
+        }
+    }
+}
+
+impl Default for WallPoints {
+    fn default() -> Self {
+        WallPoints {
+            line: None,
+            pillars: Vec::new(),
+            points: Vec::new(),
+            connections: Vec::new(),
+        }
+    }
+}
+
+//----components----
+#[derive(Component)]
+struct WallMesh {}
