51daa0a26d1d20a84548b1c11ea9978e36fcf719
danilo <didoherculano@gmail.com>
2023-09-16 16:39:31 -0300

    feat: picking hit point method

--- src/player_interaction/picking.rs
+++ src/player_interaction/picking.rs
@@ -31,32 +31,26 @@ impl Default for PickingData {
 }
 
 impl PickingData {
-    pub fn get_entity(self, collider_query: Query<(Entity, &BoxCollider)>) -> Option<Entity> {
+    pub fn get_entity(&self, collider_query: Query<(Entity, &BoxCollider)>) -> Option<Entity> {
         for (entity, collider) in collider_query.iter() {
-            if Self::intersects(self.ray, collider) {
+            if self.intersects(collider) {
                 return Some(entity);
             }
         }
         None
     }
 
-    pub fn get_hit_in_ground(self) -> Vec3 {
+    pub fn get_hit_in_ground(&self) -> Vec3 {
         let t = -self.ray.origin.y / self.ray.direction.y;
         let hit_position_ground = self.ray.origin + t * self.ray.direction;
 
-        return hit_position_ground;
+        hit_position_ground
     }
 
-    fn intersects(ray: Ray, collider: &BoxCollider) -> bool {
-        let transform = Transform {
-            translation: collider.translation,
-            rotation: collider.rotation,
-            scale: collider.scale,
-        };
-
+    fn calculate_tmin_tmax(&self, transform: Transform) -> Option<(f32, f32)> {
         let inv_rot = transform.rotation.inverse();
-        let local_origin = inv_rot * (ray.origin - transform.translation);
-        let local_direction = inv_rot * ray.direction;
+        let local_origin = inv_rot * (self.ray.origin - transform.translation);
+        let local_direction = inv_rot * self.ray.direction;
 
         let min = -transform.scale / 2.0;
         let max = transform.scale / 2.0;
@@ -72,11 +66,43 @@ impl PickingData {
         let tmax = t1.max(t2).min(t3.max(t4)).min(t5.max(t6));
 
         if tmin > tmax {
-            return false;
+            return None;
         }
 
-        // A interseção é válida se tmin está dentro do intervalo [0, tmax]
-        tmin >= 0.0 && tmin <= tmax
+        Some((tmin, tmax))
+    }
+
+    fn intersects(&self, collider: &BoxCollider) -> bool {
+        let transform = Transform {
+            translation: collider.translation,
+            rotation: collider.rotation,
+            scale: collider.scale,
+        };
+
+        self.calculate_tmin_tmax(transform).is_some()
+    }
+
+    pub fn get_hit_point(&self, collider_query: Query<(Entity, &BoxCollider)>) -> Option<(Vec3)> {
+        for (entity, collider) in collider_query.iter() {
+            let transform = Transform {
+                translation: collider.translation,
+                rotation: collider.rotation,
+                scale: collider.scale,
+            };
+
+            if let Some((tmin, _)) = self.calculate_tmin_tmax(transform) {
+                let inv_rot = transform.rotation.inverse();
+                let local_origin = inv_rot * (self.ray.origin - transform.translation);
+                let local_direction = inv_rot * self.ray.direction;
+
+                let hit_point_local = local_origin + tmin * local_direction;
+                let hit_point_world = transform.rotation * hit_point_local + transform.translation;
+
+                return Some(hit_point_world);
+            }
+        }
+
+        None
     }
 }
 
